d7e447240f6568d6af400406e86d6cec36e0a0a0
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-11-02 14:31:43 -0600

    Improve error checking and handling in the login process

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ if not os.path.exists(PASSTORE_PATH):
     dialog.show()
     app.exec()
     # create the user prefernces if a password database was created
-    if dialog.file_path:
+    if os.path.exists(dialog.file_path):
         os.mkdir(f'{PASSTORE_PATH}')
         with open(f'{PASSTORE_PATH}/prefs.json', 'w') as p:
             json.dump({'last_modified':  dialog.file_path}, p)
@@ -26,9 +26,19 @@ else:
     with open(f'{PASSTORE_PATH}/prefs.json') as p:
         prefs = json.load(p)
     # attempt to decrypt the password file
-    password_file =  show_login_prompt(prefs['last_modified'])
-    if password_file:
-        app = QApplication.instance()
-        main_window = MainWindow(password_file)
-        main_window.show()
+    last_modified = prefs['last_modified']
+    if os.path.exists(last_modified):
+        password_file =  show_login_prompt(last_modified)
+        if password_file:
+            app = QApplication.instance()
+            main_window = MainWindow(password_file)
+            main_window.show()
+            sys.exit(app.exec())
+    else:
+        app = QApplication()
+        err = QErrorMessage()
+        err.showMessage(f'The file "{last_modified}" could not be found. Please create a new password database.')
+        err.show()
+        dialog = FileCreationDialog()
+        dialog.show()
         sys.exit(app.exec())
