83243eb070243d1b9a96e1a04b656e0f08246f88
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-11-02 19:40:26 -0600

    Add support for selecting different password database files

--- src/gui/login.py
+++ src/gui/login.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 from PySide6.QtGui import QShortcut, QKeySequence
-from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QPushButton, QErrorMessage, QLineEdit, QApplication
+from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QPushButton, QErrorMessage, QLineEdit, QFileDialog, QApplication
 from PySide6.QtCore import Qt
 
 sys.path.append(os.path.abspath('src'))
@@ -12,15 +12,15 @@ from password_db import PasswordDatabase
 class LoginWindow(QWidget):
     def __init__(self, file_path: str):
             super().__init__()
-            self.setWindowTitle(f'Unlock {os.path.basename(file_path)}')
             self.file_path = file_path
-            self.attempts = 0 # keep track of the number of times the user has attepted to decrypt the password file
             self.enter = QShortcut(QKeySequence('Return'), self)
             self.enter.activated.connect(self.decrypt_passwords)
-            self.password_file = None
             self.init_dialog()
 
     def init_dialog(self):
+        self.setWindowTitle(f'Unlock {os.path.basename(self.file_path)}')
+        self.attempts = 0 # keep track of the number of times the user has attepted to decrypt the password file
+        self.password_file = None
         # build widgets
         self.file_label = QLabel(f'Unlock "{os.path.basename(self.file_path)}"')
         self.file_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -29,6 +29,8 @@ class LoginWindow(QWidget):
         self.pass_entry.setEchoMode(QLineEdit.EchoMode.Password)
         self.accept_button = QPushButton('Accept')
         self.accept_button.clicked.connect(self.decrypt_passwords)
+        self.file_select_button = QPushButton('Select another file')
+        self.file_select_button.clicked.connect(self.select_file)
         # this label is only given text if the user inputs an incorrect password
         self.error_label = QLabel('')
         self.error_label.setStyleSheet('color: #ff0000')
@@ -39,7 +41,8 @@ class LoginWindow(QWidget):
         self.main_layout.addWidget(self.pass_label, 1, 0, 1, 1)
         self.main_layout.addWidget(self.pass_entry, 1, 1, 1, 1)
         self.main_layout.addWidget(self.accept_button, 2, 0, 1, 2)
-        self.main_layout.addWidget(self.error_label, 3, 0, 1, 2)
+        self.main_layout.addWidget(self.file_select_button, 3, 0, 1, 2)
+        self.main_layout.addWidget(self.error_label, 4, 0, 1, 2)
         self.setLayout(self.main_layout)
 
     def decrypt_passwords(self):
@@ -56,13 +59,21 @@ class LoginWindow(QWidget):
             if self.attempts < 3:
                 self.error_label.setText(f'Incorrect Password!\n{3 - self.attempts} {"attempts" if self.attempts < 2 else "attempt"} remaining')
             else:
-                QErrorMessage(self).showMessage('An incorrect password was provided three times.\nThe program will now exit.')
+                QErrorMessage().showMessage('An incorrect password was provided three times.\nThe program will now exit.')
                 self.close()
+
+    # selects a new file to attempt to decrypt, and reinitializes the dialog for that file 
+    def select_file(self):
+        file_name = file_path = QFileDialog.getOpenFileName(filter='Password Database Files (*.pwdb)')[0]
+        if file_path: 
+            self.file_path = file_path
+            self.setWindowTitle(f'Unlock "{os.path.basename(self.file_path)}"')
+            self.file_label.setText(f'Unlock "{os.path.basename(self.file_path)}"')
+
 
 def show_login_prompt(file_path: str):
     app = QApplication(sys.argv)
     login = LoginWindow(file_path)
     login.show()
     app.exec()
-    del app
     return login.password_file
