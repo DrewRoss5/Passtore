0c0cfdd099980a18a3b29276ca3b3f8ca84ab576
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-11-05 16:16:13 -0700

    Username support

--- src/gui/edit_password.py
+++ src/gui/edit_password.py
@@ -4,11 +4,12 @@ from PySide6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QGridLayo
 from PySide6.QtCore import Qt
 
 class PasswordEditDialog(QWidget):
-    def __init__(self, parent_widget, site_name: str):
+    def __init__(self, parent_widget, password_name: str):
         super().__init__()
         self.parent_widget = parent_widget
-        self.site_name = site_name
-        self.password = self.parent_widget.password_database.get_password(site_name)
+        # split the password's name into it's components
+        self.site_name, self.username = map(lambda x: x.strip(), password_name.split('-'))
+        self.password = self.parent_widget.password_database.get_password(password_name)
         self.setWindowTitle(f'Editing password for "{self.site_name}"')
         self.init_dialog()
 
@@ -19,7 +20,10 @@ class PasswordEditDialog(QWidget):
         self.site_name_label = QLabel('Site Name:')
         self.site_name_entry = QLineEdit()
         self.site_name_entry.setText(self.site_name)
-        self.pass_label = QLabel('Password:', parent=self)
+        self.username_label = QLabel('Username:')
+        self.username_entry = QLineEdit()
+        self.username_entry.setText(self.username)
+        self.pass_label = QLabel('Password:')
         self.confirm_label = QLabel('Confirm:')
         self.pass_entry = QLineEdit(self)
         self.pass_entry.setEchoMode(QLineEdit.EchoMode.Password)
@@ -40,21 +44,24 @@ class PasswordEditDialog(QWidget):
         self.layout.addWidget(self.title_label, 0, 0, 1, 2)
         self.layout.addWidget(self.site_name_label, 1, 0, 1, 1)
         self.layout.addWidget(self.site_name_entry, 1, 1, 1, 1)
-        self.layout.addWidget(self.pass_label, 2, 0, 1, 1)
-        self.layout.addWidget(self.pass_entry, 2, 1, 1, 1)
-        self.layout.addWidget(self.confirm_label, 3, 0, 1, 1)
-        self.layout.addWidget(self.confirm_entry, 3, 1, 1, 1)
-        self.layout.addWidget(self.cancel_button, 4, 0, 1, 1)
-        self.layout.addWidget(self.confirm_button, 4, 1, 1, 1)
-        self.layout.addWidget(self.error_label, 5, 0, 1, 1)
+        self.layout.addWidget(self.username_label, 2, 0, 1, 1)
+        self.layout.addWidget(self.username_entry, 2, 1, 1, 1)
+        self.layout.addWidget(self.pass_label, 3, 0, 1, 1)
+        self.layout.addWidget(self.pass_entry, 3, 1, 1, 1)
+        self.layout.addWidget(self.confirm_label, 4, 0, 1, 1)
+        self.layout.addWidget(self.confirm_entry, 4, 1, 1, 1)
+        self.layout.addWidget(self.cancel_button, 5, 0, 1, 1)
+        self.layout.addWidget(self.confirm_button, 5, 1, 1, 1)
+        self.layout.addWidget(self.error_label, 6, 0, 1, 1)
         self.setLayout(self.layout)
 
     def confirm(self):
         password = self.pass_entry.text()
+        password_name = f'{self.site_name} - {self.username}'
         # clear the error text, if present
         self.error_label.setText('')
-        if self.site_name != self.site_name_entry.text():
-            new_name = self.site_name_entry.text()
+        if  password_name != f'{self.site_name_entry.text()} - {self.username_entry.text()}':
+            new_name = f'{self.site_name_entry.text()} - {self.username_entry.text()}'
             renamed = True
         else:
             new_name = None
@@ -67,9 +74,9 @@ class PasswordEditDialog(QWidget):
         if password != self.confirm_entry.text():
             self.error_label.setText('Password and confirmation must match')
             return 
-        self.parent_widget.password_database.update_password(self.site_name, password)
+        self.parent_widget.password_database.update_password(password_name, password)
         if renamed:
-            self.parent_widget.password_database.rename_password(self.site_name, new_name)
+            self.parent_widget.password_database.rename_password(password_name, new_name)
         self.parent_widget.update_password_list()
         self.close()
 
