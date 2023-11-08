f2fb30cd0f6d61d6dbfedb93dd8a01233b0c7807
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-11-07 23:36:43 -0700

    Show the strength of the password

--- src/gui/create_password.py
+++ src/gui/create_password.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QGridLayout, QSlider
+from PySide6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QGridLayout, QSlider, QProgressBar
 from PySide6.QtCore import Qt
 
 sys.path.append('src')
@@ -25,22 +25,24 @@ class PasswordCreationDialog(QWidget):
         self.confirm_label = QLabel('Confirm:')
         self.pass_entry = QLineEdit()
         self.pass_entry.setEchoMode(QLineEdit.EchoMode.Password)
+        self.pass_entry.textChanged.connect(self.update_strength_meter)
         self.confirm_entry = QLineEdit()
         self.confirm_entry.setEchoMode(QLineEdit.EchoMode.Password)
+        self.strength_bar = QProgressBar()
+        self.strength_label = QLabel('Strength:')
+        self.strength_bar.setRange(0, 50)
         self.slider_name_label = QLabel('Password Size:')
         self.slider_name_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.size_slider = QSlider(Qt.Orientation.Horizontal)
         self.size_slider.setMinimum(8)
         self.size_slider.setMaximum(24)
         self.size_slider.setValue(16)
-        self.size_slider.valueChanged.connect(self.update_slider)
+        self.size_slider.valueChanged.connect(self.update_password)
         self.size_label = QLabel('16', parent=self)
         self.accept_button = QPushButton('Accept', parent=self)
         self.accept_button.clicked.connect(self.create_password)
         self.cancel_button = QPushButton('Cancel', parent=self)
         self.cancel_button.clicked.connect(self.close)
-        # generate a password
-        self.generate_password(16)
         # this label is only given text if the user provides invalid input
         self.error_label = QLabel('', parent=self)
         self.error_label.setStyleSheet('color: #ff0000')
@@ -56,11 +58,13 @@ class PasswordCreationDialog(QWidget):
         self.layout.addWidget(self.pass_entry, 3, 1, 1, 1)
         self.layout.addWidget(self.confirm_label, 4, 0, 1, 1)
         self.layout.addWidget(self.confirm_entry, 4, 1, 1, 1)
-        self.layout.addWidget(self.slider_name_label, 5, 0, 1, 3)
-        self.layout.addWidget(self.size_slider, 6, 0, 1, 2)
-        self.layout.addWidget(self.size_label, 6, 2, 1, 1)
-        self.layout.addWidget(self.cancel_button, 7, 0, 1, 1)
-        self.layout.addWidget(self.accept_button, 7, 1, 2, 1)
+        self.layout.addWidget(self.strength_label, 5, 0, 1, 1)
+        self.layout.addWidget(self.strength_bar, 5, 1, 1, 2)
+        self.layout.addWidget(self.slider_name_label, 6, 0, 1, 3)
+        self.layout.addWidget(self.size_slider, 7, 0, 1, 2)
+        self.layout.addWidget(self.size_label, 7, 2, 1, 1)
+        self.layout.addWidget(self.cancel_button, 8, 0, 1, 1)
+        self.layout.addWidget(self.accept_button, 8, 1, 2, 1)
         self.setLayout(self.layout)
 
     # create the new password and save it to the password database
@@ -87,20 +91,25 @@ class PasswordCreationDialog(QWidget):
         self.site_name_entry.setText('')
         self.username_entry.setText('')
         self.size_slider.setValue(16)
-        self.generate_password(16)
+        self.update_password()
         # update the parent widget's UI
         self.parent_widget.password_database.add_password(site_name, username, password)
         self.parent_widget.update_password_list()
         self.close()
 
-    # generates a random password of a user-specified size
-    def generate_password(self, size: int):
-        password = PasswordDatabase.generate_password(size)
+    # updates the text to the QSlider, and generates a new password of an appropriate size
+    def update_password(self):
+        pass_size = self.size_slider.value()
+        self.size_label.setText(str(pass_size))
+        # generate a new passsword and place into the entries
+        password = PasswordDatabase.generate_password(pass_size)
         self.pass_entry.setText(password)
         self.confirm_entry.setText(password)
 
-    # updates the text to the QSlider, and generates a new password of an appropriate size
-    def update_slider(self):
-        pass_size = self.size_slider.value()
-        self.size_label.setText(str(pass_size))
-        self.generate_password(pass_size)
+    # updates the password strength meter
+    def update_strength_meter(self):
+        password_strength = PasswordDatabase.grade_password(self.pass_entry.text())
+        self.strength_bar.setValue(password_strength)
+        self.strength_bar.setFormat(str(password_strength))
+
+    
