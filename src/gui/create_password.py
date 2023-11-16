ba93c23737dac4c39c9eb58834805893e9f58c8c
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2023-11-15 23:26:40 -0700

    Disable confirmation button and show an error if the given password doesn't match the confirmation

--- src/gui/create_password.py
+++ src/gui/create_password.py
@@ -25,9 +25,10 @@ class PasswordCreationDialog(QWidget):
         self.confirm_label = QLabel('Confirm:')
         self.pass_entry = QLineEdit()
         self.pass_entry.setEchoMode(QLineEdit.EchoMode.Password)
-        self.pass_entry.textChanged.connect(self.update_strength_meter)
+        self.pass_entry.textChanged.connect(self.update_password)
         self.confirm_entry = QLineEdit()
         self.confirm_entry.setEchoMode(QLineEdit.EchoMode.Password)
+        self.confirm_entry.textChanged.connect(self.update_password)
         self.strength_bar = QProgressBar()
         self.strength_label = QLabel('Strength:')
         self.strength_bar.setRange(0, 50)
@@ -37,7 +38,7 @@ class PasswordCreationDialog(QWidget):
         self.size_slider.setMinimum(8)
         self.size_slider.setMaximum(24)
         self.size_slider.setValue(16)
-        self.size_slider.valueChanged.connect(self.update_password)
+        self.size_slider.valueChanged.connect(self.generate_password)
         self.size_label = QLabel('16', parent=self)
         self.accept_button = QPushButton('Accept', parent=self)
         self.accept_button.clicked.connect(self.create_password)
@@ -64,8 +65,11 @@ class PasswordCreationDialog(QWidget):
         self.layout.addWidget(self.size_slider, 7, 0, 1, 2)
         self.layout.addWidget(self.size_label, 7, 2, 1, 1)
         self.layout.addWidget(self.cancel_button, 8, 0, 1, 1)
-        self.layout.addWidget(self.accept_button, 8, 1, 2, 1)
+        self.layout.addWidget(self.accept_button, 8, 1, 1, 2)
+        self.layout.addWidget(self.error_label, 9, 0, 1, 3)
         self.setLayout(self.layout)
+        # generate a random password
+        self.generate_password()
 
     # create the new password and save it to the password database
     def create_password(self):
@@ -91,25 +95,33 @@ class PasswordCreationDialog(QWidget):
         self.site_name_entry.setText('')
         self.username_entry.setText('')
         self.size_slider.setValue(16)
-        self.update_password()
+        self.generate_password()
         # update the parent widget's UI
         self.parent_widget.password_database.add_password(site_name, username, password)
         self.parent_widget.update_password_list()
         self.close()
 
     # updates the text to the QSlider, and generates a new password of an appropriate size
-    def update_password(self):
+    def generate_password(self):
         pass_size = self.size_slider.value()
         self.size_label.setText(str(pass_size))
         # generate a new passsword and place into the entries
         password = PasswordDatabase.generate_password(pass_size)
         self.pass_entry.setText(password)
         self.confirm_entry.setText(password)
+        self.update_password()
 
-    # updates the password strength meter
-    def update_strength_meter(self):
+    def update_password(self):
+        # update the password strength meter
         password_strength = PasswordDatabase.grade_password(self.pass_entry.text())
         self.strength_bar.setValue(password_strength)
         self.strength_bar.setFormat(str(password_strength))
+        # ensure the password and confirmation match
+        if (self.pass_entry.text() != self.confirm_entry.text()):
+            self.error_label.setText('Password and Confirmation must match')
+            self.accept_button.setEnabled(False)
+        else:
+            self.error_label.setText('')
+            self.accept_button.setEnabled(True)
 
     
